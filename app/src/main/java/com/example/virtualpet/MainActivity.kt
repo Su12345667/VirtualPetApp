bedd0fc48beb5260d1fb4f024b6872c3200f931e
Su12345667 <163244104+Su12345667@users.noreply.github.com>
2024-04-23 08:37:17 +0200

    Second

--- app/src/main/java/com/example/virtualpet/MainActivity.kt
+++ app/src/main/java/com/example/virtualpet/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.virtualpet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<Button>(R.id.startBtn)
+
+        startBtn.setOnClickListener {
+            startActivity(Intent(this,MainActivity2::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/virtualpet/MainActivity2.kt
+++ app/src/main/java/com/example/virtualpet/MainActivity2.kt
@@ -2,10 +2,36 @@ package com.example.virtualpet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        private var Happiness  = 100
+        private var Hunger = 0
+        private var Cleanliness = 100
+
+        val feedBtn = findViewById<Button>(R.id.feedBtn)
+        val playBtn =findViewById<Button>(R.id.playBtn)
+        val cleanBtn = findViewById<Button>(R.id.cleanBtn)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        playBtn.setOnClickListener {
+            playWithPet()
+            imageView.setImageResource(R.drawable.img_5)
+        }
+        feedBtn.setOnClickListener {
+            feedPet()
+            imageView.setImageResource(R.drawable.img_3)
+        }
+        cleanBtn.setOnClickListener {
+            cleanPet()
+            imageView.setImageResource(R.drawable.img_4)
+        }
+
+
     }
 }
