b5138e8a3eb44f32b42dbebaa606ae164f644700
Su12345667 <163244104+Su12345667@users.noreply.github.com>
2024-04-30 16:34:38 +0200

    Third

--- app/src/main/java/com/example/virtualpet/MainActivity.kt
+++ app/src/main/java/com/example/virtualpet/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val startBtn = findViewById<Button>(R.id.startBtn)
+        val exitBtn = findViewById<Button>(R.id.exitBtn)
 
         startBtn.setOnClickListener {
             startActivity(Intent(this,MainActivity2::class.java))
+            finish()
+        }
+        exitBtn.setOnClickListener {
             finish()
         }
     }
