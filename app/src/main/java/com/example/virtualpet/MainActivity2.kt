8320faf0bd04bbf8dfa3b571fa6712d5dccc558d
Su12345667 <163244104+Su12345667@users.noreply.github.com>
2024-05-06 10:11:47 +0200

    Final

--- app/src/main/java/com/example/virtualpet/MainActivity2.kt
+++ app/src/main/java/com/example/virtualpet/MainActivity2.kt
@@ -8,15 +8,55 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
-
+    private var health = 100
+    private var hunger = 0
+    private var cleanliness = 100
+    lateinit var feedtxt : TextView
+    lateinit var healthtxt : TextView
+    lateinit var cleantxt : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        feedtxt = findViewById(R.id.hunger)
+        healthtxt = findViewById(R.id.health)
+        cleantxt = findViewById(R.id.cleanliness)
         val feedBtn = findViewById<Button>(R.id.feedBtn)
         val playBtn = findViewById<Button>(R.id.playBtn)
         val cleanBtn = findViewById<Button>(R.id.cleanBtn)
         val imageView = findViewById<ImageView>(R.id.imageView)
+
+        fun updateStatusText() {
+            healthtxt.text = "Health - $health"
+            feedtxt.text = "Hunger - $hunger"
+            cleantxt.text = "Cleanliness - $cleanliness"
+        }
+
+        fun feedPet() {
+            hunger = 20
+            if (hunger > 0) hunger = 0
+            health += 10
+            if (health > 100) health = 100
+            updateStatusText()
+        }
+
+
+        fun cleanPet() {
+            cleanliness = 100
+            health += 10
+            if (health > 100) health = 100
+            updateStatusText()
+        }
+
+        fun playWithPet() {
+            health -= 15
+            if (health > 100) health = 100
+            hunger += 15
+            if (hunger > 100) hunger = 100
+            cleanliness -= 10
+            if (cleanliness > 100) cleanliness = 100
+            updateStatusText()
+        }
 
 
         playBtn.setOnClickListener {
@@ -32,33 +72,6 @@ class MainActivity2 : AppCompatActivity() {
             imageView.setImageResource(R.drawable.img_4)
         }
     }
-    private var health = 100
-    private var hunger = 0
-    private  var cleanliness = 100
-
-    fun updateStatusText() {}
-
-    fun feedPet() {
-        hunger = 20
-        if (hunger > 0) hunger = 0
-        health += 10
-        if (health > 100) health = 100
-        updateStatusText()
-    }
+}
 
 
-        private fun cleanPet() {
-            cleanliness = 100
-            health += 10
-            if (health > 100) health = 100
-            updateStatusText()
-        }
-
-        private fun playWithPet() {
-            health -= 15
-            if (health > 0) health = 0
-            hunger += 15
-            if (hunger > 100) hunger = 100
-            updateStatusText()
-        }
-        }
